a3e0af13ae6d9594ecb766d92f4754cb7200781c
Joy <joysmith271@gmail.com>
2023-01-28 22:55:58 +0700

    redux update

--- 07 Redux/redux101/src/actions/frozenInvUpdate.js
+++ 07 Redux/redux101/src/actions/frozenInvUpdate.js
@@ -3,13 +3,13 @@
 // action is an object that has at LEAST a property of type
 // this action creator is going to be handed to the dispatch
 
-export default (operation, index)=>{
-    console.log(operation, index);
-    return {
-        type: 'updateFrozen',
-        payload: {
-            operation,
-            index
-        }
-    }
-}
+export default (operation, index) => {
+  console.log(operation, index);
+  return {
+    type: "updateFrozen",
+    payload: {
+      operation,
+      index,
+    },
+  };
+};
